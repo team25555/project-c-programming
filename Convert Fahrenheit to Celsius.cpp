6b476678f5ac083504df6ea651ac1a21228d7566
team25555 <125853565+team25555@users.noreply.github.com>
2025-10-01 23:42:44 +0700

    Add files via upload
    เขียนโปรแกรม แปลงฟาเร็นไฮน์เป็นเซลเซียส

---
+++ Convert Fahrenheit to Celsius.cpp
@@ -0,0 +1,27 @@
+// 6806021410124 ธนวิชญ์ ดอบุตร
+#include <iostream>
+using namespace std;
+
+int main() {
+    double fahrenheit, celsius;
+
+    
+    cout << "input fahrenheit (Fahrenheit): ";
+    cin >> fahrenheit;
+
+   
+    celsius = (fahrenheit - 32) * 5.0 / 9.0;
+
+    
+    cout << "Output celsius: " << celsius << " C" << endl;
+
+    return 0;
+}
+
+
+
+
+    
+
+
+    
